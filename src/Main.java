a0db7fcc1fb8f0225a5aff2ffa36101043fd2c57
Mihran Marukyan <mihran.marukyan@ukg.com>
2023-07-04 22:11:30 +0400

    3 commit from first

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("hello world");
         System.out.println("1 from first");
         System.out.println("2 from first");
+        System.out.println("3 from first");
     }
 }
