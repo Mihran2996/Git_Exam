9822b638b8fd7275de896d58cc31bc82dc3aa8c3
Mihran Marukyan <mihran.marukyan@ukg.com>
2023-07-04 22:29:23 +0400

    8 changes

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("5 from first");
         System.out.println("6 from first");
         System.out.println("7 from first");
+        System.out.println("8 from first");
     }
 }
